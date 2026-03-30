f28df2aed9156f5b3b0e39abc12d06fad955ca3c
Nitin yadav <nitinyadavtrinet@gmail.com>
2026-03-31 00:26:36 +0530

    Render issue

--- server/index.js
+++ server/index.js
@@ -10,23 +10,22 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/complaints", require("./routes/complaintRoutes"));
 
-app.use(cors());
-
+// MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Mongo Connected");
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("Mongo Error:", err);
-    process.exit(1);
+    console.log("Mongo Error:", err.message); // ❌ no exit
   });
