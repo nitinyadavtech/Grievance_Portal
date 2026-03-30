d054fc35d29509fd937d26996a872ad70f9e3d40
Nitin yadav <nitinyadavtrinet@gmail.com>
2026-03-31 01:00:06 +0530

    Fix refresh routing on Render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Complaint from "./pages/Complaint";
@@ -17,7 +17,7 @@ const AdminRoute = ({ children }) => {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
 
@@ -46,6 +46,6 @@ export default function App() {
 
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
